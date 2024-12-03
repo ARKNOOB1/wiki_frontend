6212c4cf9decb381dcdc10f77be454e33aa95b83
ARKNOOB1 <a59114154@gmail.com>
2024-12-03 20:17:22 +0900

    12_03

--- src/WikiPage.js
+++ src/WikiPage.js
@@ -1,12 +1,44 @@
 import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
 
 function WikiPage () {
     const {searchQuery} = useParams();
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`/api/creature/${searchQuery}`);
+                if(response.ok){
+                    const myData = await response.json();
+                    setData(myData);
+                }else {
+                    const errorMessage = await response.json()
+                    setError(errorMessage.message);
+                }
+            }
+            catch (error) {
+                setError("데이터를 가져오는 중 오류가 발생했습니다.")
+            }
+        };
+        fetchData();
+    }, [searchQuery])
     return(
         <div className="container">
             <h1>{searchQuery}</h1>
             <div className="article-container">
-                <h2>내용이 없습니다.</h2>
+                {error ? (
+                    <p>{error}</p>
+                ) : data ? (
+                    <div>
+                        <p>{data.시설외부생명체}</p>
+                        <p>{data.시설내부생명체}</p>
+                    </div>
+                ) : (
+                    <p>데이터를 로드 중입니다.</p>
+                )
+            } 
             </div>
         </div>
     )
